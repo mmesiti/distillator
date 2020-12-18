cb21f3b4d1123a4c9984c42bcda1dc86f048449c
Michele Mesiti <michele.mesiti@gmail.com>
2020-12-18 17:27:21 +0000

    Sombrero remote now be remote, not on my fsystem

--- remotes.sh
+++ remotes.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 export HIREP_REMOTE=git@github.com:sa2c/HiRep
 export HIREP_REMOTE_BRANCH=spn-swansea-merge
 
-export SOMBRERO_REMOTE=/home/michele/hirep-workspace/gantry_sombrero/sombrero
-export SOMBRERO_REMOTE_BRANCH=test
+export SOMBRERO_REMOTE=git@github.com:mmesiti/sombrero.git
+export SOMBRERO_REMOTE_BRANCH=thin
 
 export PYCPARSER_REMOTE=git@github.com:eliben/pycparser.git
 
