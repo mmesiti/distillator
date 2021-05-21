7b0f4f23fb750baf3604059723bcb86cd4e79e3a
Ed Bennett <e.j.bennett@swansea.ac.uk>
2021-05-21 14:54:31 +0100

    use sa2c repo for thin sombrero

--- remotes.ssh.sh
+++ remotes.ssh.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export HIREP_REMOTE=git@github.com:sa2c/HiRep
 export HIREP_REMOTE_BRANCH=spn-swansea-merge
 
-export SOMBRERO_REMOTE=git@github.com:mmesiti/sombrero.git
+export SOMBRERO_REMOTE=git@github.com:sa2c/sombrero.git
 export SOMBRERO_REMOTE_BRANCH=thin
 
 export PYCPARSER_REMOTE=git@github.com:eliben/pycparser.git
